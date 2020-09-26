6f94ca03a0db6508fcea0ce1105b2eb1d4b576cc
Jose Gudañupe Cortes Cruz <jose17y10@gmail.com>
2020-09-25 20:23:44 -0500

    APP SPA

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jsj'`, () => {
+  it(`should have as title 'sga'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jsj');
+    expect(app.title).toEqual('sga');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jsj app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('sga app is running!');
   });
 });
